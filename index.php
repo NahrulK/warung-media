7083187b505c7799127f4f55d5265f7eaff306db
unknown <khayattullah73@gmail.com>
2022-01-09 06:33:16 +0800

    firt commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Warung Media</title>
+</head>
+<body>
+<!--  Main Container -->
+<div class="main_wrapper">
+
+    <div class="header_logo">
+    Header Logo
+    </div>
+    <div class="content_wrapper">
+    Content Wrapper
+    </div><!--Content Wrapper-->
+    <div class="footer">
+    This is Footer
+    </div><!--End Footer-->
+</div><!-- End Main Wrapper-->
+<!-- End Main Container -->
+    
+</body>
+</html>
