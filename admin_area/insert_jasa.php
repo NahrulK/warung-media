4a4dc0a8d5d91f40b023c3d57e54521c06335b90
NahrulK <khayattullah73@gmail.com>
2022-01-10 15:47:11 +0800

    Dynamic Content

--- admin_area/insert_jasa.php
+++ admin_area/insert_jasa.php
@@ -24,7 +24,7 @@ include ("includes/db.php");
             </tr>
             <tr>
                 <td align="right"><b>Product Category:</b></td>
-                <td><select name="product_cat" id="">
+                <td><select name="product_cats" id="">
                     <option>Select Category</option>
                     <?php
                     
@@ -65,7 +65,7 @@ include ("includes/db.php");
             </tr>
 
             <tr>
-                <td align="right"><b>Product Category:</b></td>
+                <td align="right"><b>Product images:</b></td>
                 <td><input type="file" name="product_image"></td>
             </tr>
                     <!--  -->
@@ -99,7 +99,7 @@ include ("includes/db.php");
 
 if (isset($_POST['insert_post'])){
     $product_title = $_POST['product_title'];
-    $product_cat = $_POST['product_cat'];
+    $product_cats = $_POST['product_cats'];
     $product_brand = $_POST['product_brand'];
     $product_price = $_POST['product_price'];
     $product_keywords = $_POST['product_keywords'];
@@ -111,7 +111,7 @@ if (isset($_POST['insert_post'])){
 
     move_uploaded_file($product_image_tmp, "product_images/$product_image");
 
-    $insert_product = "insert into product (product_cat,product_brand,product_title,product_price,product_desc, product_image, product_keywords) values ('$product_title','$product_cat','$product_brand','$product_price','$product_keywords','$product_desc','$product_image')";
+    $insert_product = "insert into product (product_cat,product_brand,product_title,product_price,product_desc, product_image, product_keywords) values ('$product_cats','$product_brand','$product_title','$product_price','$product_desc','$product_image','$product_keywords')";
 
     $insert_pro = mysqli_query($con, $insert_product);
 
