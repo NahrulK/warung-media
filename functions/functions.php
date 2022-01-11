853e39011d53ff5f3723f7233492e1283b8f5533
NahrulK <khayattullah73@gmail.com>
2022-01-11 14:51:49 +0800

    Shorting produck berdasarkan ID

--- functions/functions.php
+++ functions/functions.php
@@ -45,4 +45,131 @@ function getPartner(){
     
 }
 
+function getPro() {
+
+    if(!isset($_GET['cat'])){
+        if(!isset($_GET['brand'])){
+
+            global $con;
+
+    $get_pro = "SELECT * from product order by RAND() LIMIT 0,6";
+    $run_pro = mysqli_query($con, $get_pro);
+
+    while($row_pro = mysqli_fetch_array($run_pro)) {
+
+        $pro_id = $row_pro['product_id'];
+        $pro_cat = $row_pro['product_cat'];
+        $pro_brand = $row_pro['product_brand'];
+        $pro_title = $row_pro['product_title'];
+        $pro_price = $row_pro['product_price'];
+        $pro_image = $row_pro['product_image'];
+
+        echo "
+        <div id='single_product'>
+            <h3>$pro_title</h3>
+            <img src='admin_area/product_images/$pro_image' width='180' height='180'>
+            <p><b>Price : $pro_price</b></p>
+            <a href='details.php?pro_id=$pro_id'>Details</a>
+            <a href='details.php?add_cart=$pro_id'>
+                <button style='float:right'> Add to Chart</button>
+            </a>
+        </div>
+        ";
+
+
+    }
+    }
+    }
+
+}
+
+function getProByCat() {
+    global $con;
+    if(isset($_GET['cat'])){
+        $cat_id = $_GET['cat'];
+        
+        $get_cat_pro = "SELECT * from product where product_cat='$cat_id'";
+
+        $run_cat_pro = mysqli_query($con, $get_cat_pro);
+
+        $count_cats = mysqli_num_rows($run_cat_pro);
+
+        if ($count_cats == 0){
+            echo ("<h2 style='padding: 20px;'>Tidak Ada Produk dengan Kategori ini ditemukan</h2>");
+            
+        } 
+
+        while ($row_cat_pro = mysqli_fetch_array($run_cat_pro)){
+            $pro_id = $row_cat_pro['product_id'];
+
+            $pro_cat = $row_cat_pro['product_cat'];
+
+            $pro_brand = $row_cat_pro['product_brand'];
+            $pro_title = $row_cat_pro['product_title'];
+            $pro_price = $row_cat_pro['product_price'];
+            $pro_image = $row_cat_pro['product_image']; 
+
+            echo "
+            
+            <div id='single_product'>
+                <h3>$pro_title</h3>
+                <img src='admin_area/product_images/$pro_image' width='180' height='180'>
+                <p><b>Price : $pro_price</b></p>
+                <a href='details.php?pro_id=$pro_id'>Details</a>
+                <a href='details.php?add_cart=$pro_id'>
+                    <button style='float:right'> Add to Chart</button>
+                </a>
+            </div>
+
+            ";
+        }
+        
+    } 
+} 
+
+function getProByBrand(){
+    global $con;
+
+    if(isset($_GET['brand'])){
+        $brand_id = $_GET['brand'];
+        
+        $get_brand_pro = "SELECT * from product where product_brand='$brand_id'";
+
+        $run_brand_pro = mysqli_query($con, $get_brand_pro);
+
+        $count_brands = mysqli_num_rows($run_brand_pro);
+
+        if ($count_brands == 0){
+            echo ("<h2 style='padding: 20px;'>Tidak Ada Produk dengan Kategori ini ditemukan</h2>");
+            
+        } 
+
+        while ($row_brand_pro = mysqli_fetch_array($run_brand_pro)){
+            $pro_id = $row_brand_pro['product_id'];
+
+            $pro_cat = $row_brand_pro['product_cat'];
+
+            $pro_brand = $row_brand_pro['product_brand'];
+            $pro_title = $row_brand_pro['product_title'];
+            $pro_price = $row_brand_pro['product_price'];
+            $pro_image = $row_brand_pro['product_image']; 
+
+            echo "
+            
+            <div id='single_product'>
+                <h3>$pro_title</h3>
+                <img src='admin_area/product_images/$pro_image' width='180' height='180'>
+                <p><b>Price : $pro_price</b></p>
+                <a href='details.php?pro_id=$pro_id'>Details</a>
+                <a href='details.php?add_cart=$pro_id'>
+                    <button style='float:right'> Add to Chart</button>
+                </a>
+            </div>
+
+            ";
+        }
+        
+    } 
+}
+
 ?>
--- index.php
+++ index.php
@@ -81,42 +81,21 @@ include ("includes/db.php");
            </div>
 
            <div id="content_area">
-               <div id="products">
+               <div id="product_box">
 
                 
                 <?php
-                
-                $get_pro = "SELECT * from product order by RAND() LIMIT 0,6";
-                $run_pro = mysqli_query($con, $get_pro);
-
-                while($row_pro = mysqli_fetch_array($run_pro)) {
-
-                    $pro_id = $row_pro['product_id'];
-                    $pro_cat = $row_pro['product_cat'];
-                    $pro_brand = $row_pro['product_brand'];
-                    $pro_title = $row_pro['product_title'];
-                    $pro_price = $row_pro['product_price'];
-                    $pro_image = $row_pro['product_image'];
-
-                    echo "
-                    <div id='single_product'>
-                        <h3>$pro_title</h3>
-                        <img src='admin_area/product_images/$pro_image' width='180' height='180'>
-                        <p><b>Price : $pro_price</b></p>
-                        <a href='details.php?pro_id=$pro_id'>Details</a>
-                        <a href='details.php?add_cart=$pro_id'>
-                            <button style='float:right'> Add to Chart</button>
-                        </a>
-                    </div>
-                    ";
-
-
-                };
-
+                getPro()
                 ?>
-                
-                
+                            
+                <?php
+                getProByCat();                                
+                ?> <!--Sort berdasarkan category-->
 
+                <?php
+                getProByBrand();                       
+                ?> <!--Sort berdasarkan brand-->
+                               
                </div>
            </div>
 
